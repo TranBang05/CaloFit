2c97969bdbeef10a7ca75a75c4b0294693f33036
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-06-08 22:23:37 +0700

    conflict2

--- CaloFitAPI/Dto/Response/DietResponse.cs
+++ CaloFitAPI/Dto/Response/DietResponse.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using CaloFitAPI.Models;
 
 namespace CaloFitAPI.Dto.Response
@@ -7,11 +6,5 @@ namespace CaloFitAPI.Dto.Response
     {
         public string? DietName { get; set; }
         public List<MenuResponse>? Menus { get; set; }
-=======
-﻿namespace CaloFitAPI.Dto.Response
-{
-    public class DietResponse
-    {
->>>>>>> 6ac9efca45847d6535e3c4f35e61985ef3d518a7
     }
 }
