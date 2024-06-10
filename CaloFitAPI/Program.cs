f3ed75d7bb567d51bb57831de08c907b92736b27
Hiep1210 <nguyenhoanghiep1210003@gmail.com>
2024-06-10 23:12:47 +0700

    Revert "Just a Test"
    This reverts commit 4565dd922e0c1aaabdbc88587e17c1972b52aa73.

--- CaloFitAPI/Program.cs
+++ CaloFitAPI/Program.cs
@@ -7,36 +7,39 @@ using CaloFitAPI.Service.Impl;
 using Microsoft.AspNetCore.OData;
 using Microsoft.EntityFrameworkCore;
 using Newtonsoft.Json;
+using static System.Net.Mime.MediaTypeNames;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews() // Add MVC services
-    .AddOData(opt => opt
-        .Select()
-        .Expand()
-        .Filter()
-        .OrderBy()
-        .SetMaxTop(100)
-    )
-    .AddNewtonsoftJson(options =>
-        options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
+
+builder.Services.AddControllers().AddOData(opt => opt
+    .Select()
+    .Expand()
+    .Filter()
+    .OrderBy()
+    .SetMaxTop(100)
+    ).AddNewtonsoftJson(options =>
+    options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Uncomment and configure your services as needed
-//builder.Services.AddScoped<ILogins, Login>();
-//builder.Services.AddScoped<ISignup, SignUp>();
-//builder.Services.AddScoped<Iusermanagement, userManagement>();
-//builder.Services.AddScoped<INutritionalAnalysis, NutritionalAnalysis>();
-//builder.Services.AddScoped<ICreateMeal, CreateMeal>();
-//builder.Services.AddScoped<Iforgotpass, ForgotPass>();
-//builder.Services.AddScoped<IDietRepository, DietRepository>();
-//builder.Services.AddScoped<IDietService, DietService>();
-//builder.Services.AddAutoMapper(typeof(MyMapper).Assembly);
 
+builder.Services.AddScoped<ILogins, Login>();
+builder.Services.AddScoped<ISignup, SignUp>();
+builder.Services.AddScoped<Iusermanagement, userManagement>();
+
+builder.Services.AddScoped<INutritionalAnalysis, NutritionalAnalysis>();
+
+builder.Services.AddScoped<ICreateMeal, CreateMeal>();
+builder.Services.AddScoped<Iforgotpass, ForgotPass>();
+=======
+builder.Services.AddScoped<ITest, Test>();
+builder.Services.AddScoped<IDietRepository, DietRepository>();
+builder.Services.AddScoped<IDietService, DietService>();
+builder.Services.AddAutoMapper(typeof(MyMapper).Assembly);
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -46,18 +49,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-
-app.UseRouting();
-
 app.UseAuthorization();
 
-app.UseEndpoints(endpoints =>
-{
-    endpoints.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}");
-});
+app.MapControllers();
 
 app.Run();
